d801b79841c4ef521c1626ee9c4e225cb5816e60
Rudolf Manusadzhian <rud.manusachi@gmail.com>
2019-05-31 21:31:49 -0700

    improved naming commented code a little bit

--- shortest_chain.exs
+++ shortest_chain.exs
@@ -10,38 +10,40 @@ defmodule ShortestChain do
   :f------:g--------
 
   graph = %{
-      :a => [:b, :c, :d, :h],
-      :b => [:a, :c, :d, :e],
-      :c => [:a, :b],
-      :d => [:a, :b],
-      :e => [:b, :f],
-      :f => [:e, :g, :i],
-      :g => [:f, :h],
-      :h => [:a, :g],
-      :i => [:f]
-    }
+    :a => [:b, :c, :d, :h],
+    :b => [:a, :c, :d, :e],
+    :c => [:a, :b],
+    :d => [:a, :b],
+    :e => [:b, :f],
+    :f => [:e, :g, :i],
+    :g => [:f, :h],
+    :h => [:a, :g],
+    :i => [:f]
+  }
 
-  find_path(graph, :a, :e) # => [:a, :b, :e]
+  find_min_path(graph, :a, :e) # => [:a, :b, :e]
   """
-  def find_path(graph, a, b), do: find_path(graph, a, b, [a])
+  def find_min_path(graph, a, b), do: find_min_path(graph, a, b, [a])
 
-  def find_path(graph, a, b, path) do
+  def find_min_path(graph, a, b, path) do
+    # get the list of friends of `a` and leave only ones that are not in `path` yet (avoid cycling)
     new_friends = Map.get(graph, a) -- path
 
     cond do
+      # base case
       b in new_friends ->
         Enum.reverse([b | path])
 
       [] == new_friends ->
-        []
+        :no_new_friends
 
       true ->
         new_friends
-        |> Enum.map(fn friend -> find_path(graph, friend, b, [friend | path]) end)
+        |> Enum.map(fn friend -> find_min_path(graph, friend, b, [friend | path]) end)
         |> Enum.reduce(fn
-          shortest, [] -> shortest
-          shortest, path when length(shortest) < length(path) -> shortest
-          _, path -> path
+          shortest_path, :no_new_friends -> shortest_path
+          shortest_path, path when length(shortest_path) < length(path) -> shortest_path
+          _shortest_path, path -> path
         end)
     end
   end
@@ -65,11 +67,11 @@ defmodule ShortestChainTest do
   }
 
   test "simple case" do
-    assert ShortestChain.find_path(@graph, :a, :b) == [:a, :b]
+    assert ShortestChain.find_min_path(@graph, :a, :b) == [:a, :b]
   end
 
   test "friend of friend" do
-    assert ShortestChain.find_path(@graph, :a, :i) == [:a, :b, :e, :f, :i]
-    assert ShortestChain.find_path(@graph, :a, :g) == [:a, :h, :g]
+    assert ShortestChain.find_min_path(@graph, :a, :i) == [:a, :b, :e, :f, :i]
+    assert ShortestChain.find_min_path(@graph, :a, :g) == [:a, :h, :g]
   end
 end
